3ce727953db1e4096cb28f91360911fa6de3b674
shyarnis <shyarnisgg88@gmail.com>
2023-07-19 23:40:12 +0545

    implement lexer for tiny compiler through blog

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # teeny-tiny-compiler
 
-- [Part 1](https://austinhenley.com/blog/teenytinycompiler1.html)
+- [Part 1](https://austinhenley.com/blog/teenytinycompiler1.html) Lexer Implementation (Lexical Analysis or Scanning)
+    - **Input**: Source Code 
+    - **Output**: Token with token type (stream of tokens)
+
--- teenytiny.py
+++ teenytiny.py
@@ -1,11 +1,19 @@
-from lex import Lexer
+from lex import Lexer, TokenType
 
 def main():
-    source_code = "Hello World 343"
-    lexer = Lexer(source_code)
+    # source_code = "+ - * /"
+    # source_code = "+ - * / > >= = != !"
+    # source_code = "#This will be ignored\n"
+    # source_code = "\"String = 13\" != +"
+    # source_code = "2+3=5"
+    # source_code = "2.45 + 3.55 = 6.00"
+    # source_code = "IF foo THEN bar"
+    source_code = "PRINT\"Hello, World\" > 4.5"
 
-    # check upto last character
-    while lexer.peek() != '\0':
-        print(lexer.curChar)
-        lexer.nextChar()        # Next Character
+    lexer = Lexer(source_code)
+    
+    token = lexer.getToken()
+    while token.kind != TokenType.EOF:
+        print(token.kind)
+        token = lexer.getToken()
 main()
